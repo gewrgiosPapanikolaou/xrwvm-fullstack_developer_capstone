d2a40efeb07d0a249b0238b3da5cf64d1e6b27d0
Gewrgios Papanikolaou <gewrgios.papanikolaou+1@gmail.com>
2024-12-08 04:25:42 -0500

    models

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,16 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+# from django.shortcuts import render
+# from django.http import HttpResponseRedirect, HttpResponse
+# from django.contrib.auth.models import User
+# from django.shortcuts import get_object_or_404, render, redirect
+# from django.contrib.auth import logout
+# from django.contrib import messages
+# from datetime import datetime
+# from .models import CarMake, CarMode
+# from django.http import JsonResponse
+# from django.contrib.auth import login, authenticate
+# import logging
+# import json
+# from django.views.decorators.csrf import csrf_exempt
+# from .populate import initiate
+# Uncomment the required imports before adding the code
+
+# from django.shortcuts import render
+# from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
+
+# from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
+
+# from django.contrib import messages
+# from datetime import datetime
+from .models import CarMake, CarModel
+# from .restapis import get_request, analyze_review_sentiments, post_review
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
 import json
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 
 # from .models import CarMake, CarModel
@@ -83,6 +103,16 @@ def registration(request):
     else :
         data = {"userName":username,"error":"Already Registered"}
         return JsonResponse(data)
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
